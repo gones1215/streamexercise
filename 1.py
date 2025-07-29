d34bfb6f6b414ea675480fed926e5f92c64e32df
gones1215 <46929085+gones1215@users.noreply.github.com>
2025-07-29 11:58:28 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,28 +1,9 @@
-import numpy as np
-import altair as alt
-import pandas as pd
 import streamlit as st
+from datetime import time, datetime
 
-st.header('부산강서고 여름방학 파이썬 보충')
+st.header('st.slider')
 
-st.write('hello, 강서고등학교')
+st.subheader('Slider')
 
-df = pd.DataFrame({
-    '첫번째 칼럼' : [1, 2, 3, 4],
-    '두 번째 칼럼' : [10, 20, 30, 40]
-})
-
-st.write(df)
-
-st.write('아래는 DataFrame입니다. ', df, '위는 dataframe입니다. ')
-
-df2 = pd.DataFrame(
-    np.random.randn(200,3),
-    columns=['a', 'b', 'c']
-)
-
-c = alt.Chart(df2).mark_circle().encode(
-    x = 'a', y = 'b', size = 'c', color = 'c', tooltip= ['a', 'b', 'c']
-)
-
-st.write(c)
+age = st.slider('당신의 나이는?', 0, 130, 25)
+st.write("나는", age, '살입니다.')
